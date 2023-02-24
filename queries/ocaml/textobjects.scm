bfd3517d42b6c8e84d54819aa00a2a5368fb1f2e
emilienlemaire <emilien.lem@icloud.com>
2023-02-24 13:36:17 +0100

    Update

--- queries/ocaml/textobjects.scm
+++ queries/ocaml/textobjects.scm
@@ -1,11 +1,5 @@
-(fun_expression
-  body: (_) @function.inner) @function.outer
-
 (let_binding
-  (_)
-  (parameter)+
-  body: (_) @function.inner) @function.outer
-
-(let_binding
-  (_)
   body: (_) @let.inner) @let.outer
+
+(parenthesized_expression ("begin" (_) @begin.inner "end")) @begin.outer
+
